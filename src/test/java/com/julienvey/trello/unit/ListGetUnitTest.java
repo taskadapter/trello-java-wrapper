bfa2313a15edbdbbf5ee21094a42be217ff68955
Julien Vey <vey.julien@gmail.com>
2013-08-26 12:10:26 +0200

    Tests on Get list

--- src/test/java/com/julienvey/trello/unit/ListGetUnitTest.java
+++ src/test/java/com/julienvey/trello/unit/ListGetUnitTest.java
@@ -2,6 +2,8 @@ package com.julienvey.trello.unit;
 
 import com.julienvey.trello.Trello;
 import com.julienvey.trello.TrelloHttpClient;
+import com.julienvey.trello.domain.Attachment;
+import com.julienvey.trello.domain.Card;
 import com.julienvey.trello.domain.TList;
 import com.julienvey.trello.impl.TrelloImpl;
 import org.fest.assertions.Assertions;
@@ -11,11 +13,13 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.*;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class ListGetUnitTest {
-
-    private static final String TEST_APPLICATION_KEY = "db555c528ce160c33305d2ea51ae1197";
-    public static final String LIST_ID = "518baad5b05dbf4703004853";
 
     private Trello trello;
 
@@ -24,14 +28,26 @@ public class ListGetUnitTest {
     @Before
     public void setUp() {
         httpClient = Mockito.mock(TrelloHttpClient.class);
-        trello = new TrelloImpl(TEST_APPLICATION_KEY, "", httpClient);
+        trello = new TrelloImpl("", "", httpClient);
     }
 
     @Test
     public void testGetList(){
-        TList list = trello.getList(LIST_ID);
+        //Given
+        TList mockList = new TList();
+        mockList.setId("idList");
 
+        when(httpClient.get(anyString(), any(Class.class), (String[]) anyVararg())).thenReturn(mockList);
+
+        //When
+        TList list = trello.getList("idList");
+
+        //Then
         assertThat(list).isNotNull();
-        assertThat(list.getId()).isEqualTo(LIST_ID);
+        assertThat(list.getId()).isEqualTo("idList");
+
+        verify(httpClient).get(eq("https://api.trello.com/1/lists/{listId}?key={applicationKey}&token={userToken}"),
+                eq(TList.class), eq("idList"), eq(""), eq(""));
+        verifyNoMoreInteractions(httpClient);
     }
 }
