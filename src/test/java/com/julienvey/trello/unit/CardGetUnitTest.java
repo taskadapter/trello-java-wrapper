dc268c15328f629a7d928f18e2588220ed66ab6f
Benjamin Holmes <benjamin.holmes@gmail.com>
2016-03-11 17:54:37 +0000

    reverted eroneous test refactoring

--- src/test/java/com/julienvey/trello/unit/CardGetUnitTest.java
+++ src/test/java/com/julienvey/trello/unit/CardGetUnitTest.java
@@ -2,23 +2,21 @@ package com.julienvey.trello.unit;
 
 import com.julienvey.trello.Trello;
 import com.julienvey.trello.TrelloHttpClient;
-import com.julienvey.trello.domain.Action;
-import com.julienvey.trello.domain.Attachment;
-import com.julienvey.trello.domain.Board;
-import com.julienvey.trello.domain.Card;
+import com.julienvey.trello.domain.*;
 import com.julienvey.trello.impl.TrelloImpl;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Matchers.*;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class CardGetUnitTest {
 
