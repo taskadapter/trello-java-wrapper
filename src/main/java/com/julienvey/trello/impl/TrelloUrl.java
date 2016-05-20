5337a2fa6e8fe9841d58a80cf28ed062c2117d3e
gustavo <gus.ehr@gmail.com>
2016-05-19 23:19:35 -0300

    Add comment to card

--- src/main/java/com/julienvey/trello/impl/TrelloUrl.java
+++ src/main/java/com/julienvey/trello/impl/TrelloUrl.java
@@ -43,6 +43,7 @@ public class TrelloUrl {
     public static final String CREATE_CARD = "/cards?pos=top&";
     public static final String GET_MEMBER = "/members/{username}?";
     public static final String ADD_LABEL_TO_CARD = "/cards/{cardId}/labels?";
+    public static final String ADD_COMMENT_TO_CARD = "/cards/{cardId}/actions/comments?";
 
     public static final String UPDATE_CARD = "/cards/{cardId}?";
 
