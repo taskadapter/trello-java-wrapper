0bdd75b3c8b361417a42af7a6d37341ae87b48f6
Edgar Asatryan <nstdio@gmail.com>
2019-02-02 18:45:21 +0400

    fixup! fixup! fixup! Merge remote-tracking branch 'origin/TJW-051' into TJW-051

--- src/test/scala/com/julienvey/trello/TrelloTestFactory.scala
+++ src/test/scala/com/julienvey/trello/TrelloTestFactory.scala
@@ -1,9 +1,9 @@
 package com.julienvey.trello
 
 import com.julienvey.trello.impl.TrelloImpl
-import com.julienvey.trello.impl.http.{ApacheHttpClient, OkHttpTrelloHttpClient}
+import com.julienvey.trello.impl.http.ApacheHttpClient
 
 object TrelloTestFactory {
-  val httpClient = new OkHttpTrelloHttpClient()
+  val httpClient = new ApacheHttpClient
   val trello = new TrelloImpl(TrelloConfig.applicationKey, TrelloConfig.token, httpClient)
 }
